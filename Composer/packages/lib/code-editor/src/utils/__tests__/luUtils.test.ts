e8b398dd0ab5f8d28c74ed7d199481d08caabd2e
TJ <tj.durnford@microsoft.com>
2021-03-21 11:15:01 -0600

    isSelectionWithinBrackets

--- Composer/packages/lib/code-editor/src/utils/__tests__/luUtils.test.ts
+++ Composer/packages/lib/code-editor/src/utils/__tests__/luUtils.test.ts
@@ -24,9 +24,19 @@ describe('luUtils', () => {
     const selection = { startColumn: 10, endColumn: 13 };
     expect(isSelectionWithinBrackets(line, selection, 'bar')).toBeFalsy();
   });
+  it('selectionContainedInBrackets: return false when selection is within escaped brackets', () => {
+    const line = '\\{bar\\}';
+    const selection = { startColumn: 3, endColumn: 5 };
+    expect(isSelectionWithinBrackets(line, selection, 'bar')).toBeFalsy();
+  });
+  it('selectionContainedInBrackets: return true when selection is within brackets', () => {
+    const line = '{bar}';
+    const selection = { startColumn: 2, endColumn: 4 };
+    expect(isSelectionWithinBrackets(line, selection, 'bar')).toBeTruthy();
+  });
   it('selectionContainedInBrackets: return true when selection contains bracket', () => {
     const line = '- foo {bar}';
     const selection = { startColumn: 5, endColumn: 9 };
-    expect(isSelectionWithinBrackets(line, selection, 'o {ba')).toBeTruthy();
+    expect(isSelectionWithinBrackets(line, selection, 'o {b')).toBeTruthy();
   });
 });
